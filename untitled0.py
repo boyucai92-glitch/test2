ffd0f2b4b949ec18e211e88f5c2c758eba1a0d37
boyucai92-glitch <boyucai92@gmail.com>
2025-08-23 20:41:15 +0800

    Create untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 22 20:51:22 2025
+
+@author: a0955779017
+"""
+a=38.5;
+
+
+b=1e+3
+
+
+print(f'1. b={b}')
+
+print (f"2. a+b={a+b}")
+
+sum=a+b
+print("a+b="+str(sum))
