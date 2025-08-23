fbbb64ec875d80131e587f8a4251eefc8d50d67c
boyucai92-glitch <boyucai92@gmail.com>
2025-08-23 20:41:25 +0800

    Create untitled1.py

---
+++ untitled1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 00:36:53 2025
+
+@author: a0955779017
+"""
+
+n1=input ("please enter a number")
+n2=input ("please enter a number")
+sum=int(n1)+int(n2)
+print (f"n1+n2={sum}")
