e286333e8e41840bace8c2e251b4efef53e459fb
boyucai92-glitch <boyucai92@gmail.com>
2025-08-24 20:40:10 +0800

    Create untitled6.py

---
+++ untitled6.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 24 20:15:01 2025
+
+@author: a0955779017
+"""
+
+
+y=0
+for i in range(5):
+    x=int(input("please enter a number"))
+    y=y+x
+print(f"the result is {y}")
