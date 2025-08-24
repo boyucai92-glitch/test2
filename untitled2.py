5ecea0e4c2e52d94c9a610196c479ce7e7c39a22
boyucai92-glitch <boyucai92@gmail.com>
2025-08-24 20:40:00 +0800

    Create untitled2.py

---
+++ untitled2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 20:42:25 2025
+
+@author: a0955779017
+"""
+
+#take input nuber for 5 times
+n1=input("please enter a number")
+sum=int(n1)+int(n1)+int(n1)+int(n1)+int(n1)
+print(f"5 times of n1 is {sum}")
