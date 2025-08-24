989bc7a46a1081aded6c089713afa567b7b2e6f6
boyucai92-glitch <boyucai92@gmail.com>
2025-08-24 20:39:56 +0800

    Create untitled3.py

---
+++ untitled3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 20:46:00 2025
+
+@author: a0955779017
+"""
+# 某個數乘5
+x=int(input("please enter a number"))
+y=0
+for i in range(5):
+    y=y+x
+print(f"the result is {y}")
