f6db7574635ac2af3747af6546bf2a7bff253a60
boyucai92-glitch <boyucai92@gmail.com>
2025-08-24 20:40:04 +0800

    Create untitled4.py

---
+++ untitled4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 23 21:39:50 2025
+
+@author: a0955779017
+"""
+#5個數加一起
+y = 0
+for i in range(5):
+    x = int(input("Enter a number: "))
+    y = y + x
+print(f"the result = {y }")
+
